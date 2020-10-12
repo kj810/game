0da0ab84046dc1550dacbf7a1efdb6d99783aa02
kj810 <kkj0810k@naver.com>
2020-10-12 18:23:33 +0900

    kj's resolute

--- KJgame.py
+++ KJgame.py
@@ -10,8 +10,9 @@ BADDIEMINSIZE = 10
 BADDIEMAXSIZE = 40
 BADDIEMINSPEED = 8
 BADDIEMAXSPEED = 8
-ADDNEWBADDIERATE = 6
+ADDNEWBADDIERATE = 8
 PLAYERMOVERATE = 5
+WALLSPEED = 8
 count = 3
 topScore=0
 
@@ -54,8 +55,34 @@ car4 = pygame.image.load('car4.png')
 playerRect = playerImage.get_rect()
 baddieImage = pygame.image.load('car2.png')
 sample = [car3, car4, baddieImage]
-wallLeft = pygame.image.load('left.png')
-wallRight = pygame.image.load('right.png')
+
+wallLeft = pygame.image.load("left.png")
+wallLeft_size = wallLeft.get_rect().size
+wallLeft_width = wallLeft_size[0]
+wallLeft_height = wallLeft_size[1]
+wallLeft_x_pos = 0
+wallLeft_y_pos = 0
+
+wallLeft2 = pygame.image.load("left.png")
+wallLeft2_size = wallLeft2.get_rect().size
+wallLeft2_width = wallLeft2_size[0]
+wallLeft2_height = wallLeft2_size[1]
+wallLeft2_x_pos = 0
+wallLeft2_y_pos = -WINDOWHEIGHT
+
+wallRight = pygame.image.load("right.png")
+wallRight_size = wallRight.get_rect().size
+wallRight_width = wallRight_size[0]
+wallRight_height = wallRight_size[1]
+wallRight_x_pos = 0
+wallRight_y_pos = 0
+
+wallRight2 = pygame.image.load("right.png")
+wallRight2_size = wallRight2.get_rect().size
+wallRight2_width = wallRight2_size[0]
+wallRight2_height = wallRight2_size[1]
+wallRight2_x_pos = 0
+wallRight2_y_pos = -WINDOWHEIGHT
 
 font = pygame.font.SysFont(None, 42)
 drawText('PRESS ANY KEY TO START THE GAME!', font, windowSurface, (WINDOWWIDTH / 3) - 137, (WINDOWHEIGHT / 3)+80)
@@ -116,6 +143,34 @@ while (count > 0):
                 if event.key == K_DOWN or event.key == ord('s'):
                     moveDown = False
 
+
+        if not reverseCheat and not slowCheat:
+            WALLSPEED = 8
+        if reverseCheat:
+            WALLSPEED = -3
+        if slowCheat:
+            WALLSPEED = 1
+            
+        wallLeft_y_pos += WALLSPEED
+        if wallLeft_y_pos > WINDOWHEIGHT:
+            wallLeft_y_pos = 0
+            wallLeft_x_pos = 0
+
+        wallRight_y_pos += WALLSPEED
+        if wallRight_y_pos > WINDOWHEIGHT:
+            wallRight_y_pos = 0
+            wallRight_x_pos = WINDOWWIDTH - wallRight_width
+       
+        wallLeft2_y_pos += WALLSPEED
+        if wallLeft2_y_pos > 0:
+            wallLeft2_y_pos = -WINDOWHEIGHT
+            wallLeft2_x_pos = 0
+
+        wallRight2_y_pos += WALLSPEED
+        if wallRight2_y_pos > 0:
+            wallRight2_y_pos = -WINDOWHEIGHT
+            wallRight2_x_pos = WINDOWWIDTH - wallRight2_width
+
         if not reverseCheat and not slowCheat:
             baddieAddCounter += 1
         if baddieAddCounter == ADDNEWBADDIERATE:
@@ -126,16 +181,17 @@ while (count > 0):
                          'surface': pygame.transform.scale(random.choice(sample), (23, 47)),
                          }
             baddies.append(newBaddie)
-            sideLeft = {'rect': pygame.Rect(0, 0, 126, 600),
-                        'speed': random.randint(BADDIEMINSPEED, BADDIEMAXSPEED),
-                        'surface': pygame.transform.scale(wallLeft, (126, 599)),
-                        }
-            baddies.append(sideLeft)
-            sideRight = {'rect': pygame.Rect(497, 0, 303, 600),
-                         'speed': random.randint(BADDIEMINSPEED, BADDIEMAXSPEED),
-                         'surface': pygame.transform.scale(wallRight, (303, 599)),
-                         }
-            baddies.append(sideRight)
+
+            # sideLeft = {'rect': pygame.Rect(0, 0, 126, 600),
+            #             'speed': 1,
+            #             'surface': pygame.transform.scale(wallLeft, (126, 599)),
+            #             }
+            # walls.append(sideLeft)
+            # sideRight = {'rect': pygame.Rect(497, 0, 303, 600),
+            #              'speed': random.randint(BADDIEMINSPEED, BADDIEMAXSPEED),
+            #              'surface': pygame.transform.scale(wallRight, (303, 599)),
+            #              }
+            # baddies.append(sideRight)
 
         if moveLeft and playerRect.left > 0:
             playerRect.move_ip(-1 * PLAYERMOVERATE, 0)
@@ -157,6 +213,8 @@ while (count > 0):
         for b in baddies[:]:
             if b['rect'].top > WINDOWHEIGHT:
                 baddies.remove(b)
+                
+
 
         font = pygame.font.SysFont(None, 38)
         windowSurface.fill(BACKGROUNDCOLOR)
@@ -165,6 +223,11 @@ while (count > 0):
         drawText('Rest Life: %s' % (count), font, windowSurface, 128, 41)
 
         windowSurface.blit(playerImage, playerRect)
+
+        windowSurface.blit(wallLeft, (wallLeft_x_pos, wallLeft_y_pos))
+        windowSurface.blit(wallRight, ((WINDOWWIDTH - wallRight_width), wallRight_y_pos) )
+        windowSurface.blit(wallLeft2, (wallLeft2_x_pos, wallLeft2_y_pos))
+        windowSurface.blit(wallRight2, ((WINDOWWIDTH - wallRight2_width), wallRight2_y_pos) )
 
         for b in baddies:
             windowSurface.blit(b['surface'], b['rect'])
@@ -175,6 +238,39 @@ while (count > 0):
             if score > topScore:
                 topScore = score
             break
+
+        wallLeft_rect = wallLeft.get_rect()
+        wallLeft_rect.left = wallLeft_x_pos
+        wallLeft_rect.top = wallLeft_y_pos
+        wallLeft2_rect = wallLeft2.get_rect()
+        wallLeft2_rect.left = wallLeft2_x_pos
+        wallLeft2_rect.top = wallLeft2_y_pos
+        wallRight_rect = wallRight.get_rect()
+        wallRight_rect.left = wallRight_x_pos
+        wallRight_rect.top = wallRight_y_pos
+        wallRight2_rect = wallRight2.get_rect()
+        wallRight2_rect.left = wallRight2_x_pos
+        wallRight2_rect.top = wallRight2_y_pos
+
+
+        if playerRect.colliderect(wallLeft_rect):
+            if score > topScore:
+                topScore = score
+            break
+        if playerRect.colliderect(wallLeft2_rect):
+            if score > topScore:
+                topScore = score
+            break
+        if playerRect.colliderect(wallRight_rect):
+            if score > topScore:
+                topScore = score
+            break
+        if playerRect.colliderect(wallRight2_rect):
+            if score > topScore:
+                topScore = score
+            break
+
+
 
         mainClock.tick(FPS)
 
